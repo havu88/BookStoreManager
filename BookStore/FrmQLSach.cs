b05b34832b64d17cafe9250b62ef11308b00fd08
NTinh <88362386+TinhNL@users.noreply.github.com>
2023-11-07 02:45:42 +0700

    Update FrmQLSach.cs

--- BookStore/FrmQLSach.cs
+++ BookStore/FrmQLSach.cs
@@ -26,6 +26,19 @@ namespace BookStore
         {
 
         }
+        
+        BindingSource dsSach = new BindingSource();
+
+        void LoadData()
+        {
+            dtgvDSSach.DataSource = dsSach;
+            LoadDSSach();
+        }
+        void LoadDSSach()
+        {
+            dsSach.DataSource = SachDAO.Instance.hienThiSach();
+        }
+
         private void refreshGD()
         {
             txtMaSach.Text = "";
@@ -35,18 +48,35 @@ namespace BookStore
             txtMaNhomSach.Text = "";
             txtTenNhomSach.Text = "";
         }
+       
         private void btnThemSach_Click(object sender, EventArgs e)
         {
+            string maSach = txtMaSach.Text;
+            string tenSach = txtTenSach.Text;
+            float donGia = Convert.ToSingle(txtDonGia.Text);
+            int tonKho = Convert.ToInt32(txtTonKho.Text);
+            string maNhomSach = txtMaNhomSach.Text;
+            string tenNhomSach = txtTenNhomSach.Text;
             if (txtMaSach.Text != "" &&  txtTenSach.Text != "" && txtDonGia.Text != "" && txtTonKho.Text != "" && txtMaNhomSach.Text != "" && txtTenNhomSach.Text != "")
             {
                 try
                 {
-                    int DonGia = Convert.ToInt32(txtDonGia.Text);
                     try
                     {
                         int TonKho = Convert.ToInt32(txtTonKho.Text);
-                        dtgvDSSach.Rows.Add(txtMaSach.Text, txtTenSach.Text, txtDonGia.Text, txtTonKho.Text, txtMaNhomSach.Text, txtTenNhomSach.Text);
-                        refreshGD();
+                        if (SachDAO.Instance.ThemSach(maSach, tenSach, donGia, tonKho, maNhomSach, tenNhomSach))
+                        {
+                            MessageBox.Show("Thêm sách thành công");
+                            LoadDSSach(); 
+                            refreshGD();
+                            if (themsach != null)
+                                themsach(this, new EventArgs());
+                        }
+                        else
+                        {
+                            MessageBox.Show("Có lỗi khi thêm sách mới");
+                        }
+                       
                     }
                     catch (FormatException) 
                     {
@@ -65,27 +95,49 @@ namespace BookStore
             else
             {
                 MessageBox.Show("Xin nhập đầy đủ thông tin!");
-            }    
+            }
         }
 
         private void btnSuaSach_Click(object sender, EventArgs e)
         {
-            int maSach = dtgvDSSach.CurrentCell.RowIndex;
-            dtgvDSSach[0, maSach].Value = txtMaSach.Text;
-            dtgvDSSach[1, maSach].Value = txtTenSach.Text;
-            dtgvDSSach[2, maSach].Value = txtDonGia.Text;
-            dtgvDSSach[3, maSach].Value = txtTonKho.Text;
-            dtgvDSSach[4, maSach].Value = txtMaNhomSach.Text;
-            dtgvDSSach[5, maSach].Value = txtTenNhomSach.Text;
-            refreshGD();
+            string maSach = txtMaSach.Text;
+            string tenSach = txtTenSach.Text;
+            float donGia = Convert.ToSingle(txtDonGia);
+            int tonKho = Convert.ToInt32(txtTonKho.Text);
+            string maNhomSach = txtMaNhomSach.Text;
+
+            if (SachDAO.Instance.SuaSach(maSach, tenSach, donGia, tonKho, maNhomSach))
+            {
+                MessageBox.Show("Sửa thông tin sách thành công");
+                LoadDSSach() ;
+                refreshGD();
+                if (suasach != null)
+                    suasach(this, new EventArgs());
+            }
+            else
+            {
+                MessageBox.Show("Có lỗi khi sửa sách");
+            }
         }
 
         private void btnXoaSach_Click(object sender, EventArgs e)
         {
-            int maSach = dtgvDSSach.CurrentCell.RowIndex;
+            string maSach = txtMaSach.Text;
             if (MessageBox.Show("Bạn có muốn xóa thông tin này", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
             {
-                dtgvDSSach.Rows.RemoveAt(maSach);
+               
+                if (SachDAO.Instance.XoaSach(maSach))
+                {
+                    MessageBox.Show("Xóa sách thành công");
+                    LoadDSSach();
+                    refreshGD();
+                    if (xoasach != null)
+                        xoasach(this, new EventArgs());
+                }
+                else
+                {
+                    MessageBox.Show("Có lỗi khi xóa sách");
+                }
             }    
         }
 
@@ -102,6 +154,37 @@ namespace BookStore
                 txtMaSach.Text = row.Cells[5].Value.ToString();
                 refreshGD();
             }
+        }
+        private event EventHandler themsach;
+        public event EventHandler Themsach
+        {
+            add { themsach += value; }
+            remove { themsach -= value; }
+        }
+
+        private event EventHandler xoasach;
+        public event EventHandler Xoasach
+        {
+            add { xoasach += value; }
+            remove { xoasach -= value; }
+        }
+
+        private event EventHandler suasach;
+        public event EventHandler Suasach
+        {
+            add { suasach += value; }
+            remove { suasach -= value; }
+        }
+
+        private void menuItemSPK_Click(object sender, EventArgs e)
+        {
+            FrmQuanLySPK  frmspk = new FrmQuanLySPK();
+            frmspk.ShowDialog();
+        }
+
+        private void menuItemHome_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
